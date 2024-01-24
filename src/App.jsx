8b5b8f1d7bf12f009ff67fc68675d72d1fe9b836
Ariy83 <143614766+Ariy83@users.noreply.github.com>
2024-01-24 20:12:05 +0200

    test-fix4

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate, HashRouter } from 'react-router-dom';
 import Home from './pages/Home';
 import Catalog from './pages/Catalog';
 import Favorite from './pages/Favorite';
 
 const App = () => {
   return (
-    <>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/catalog" element={<Catalog />} />
         <Route path="/favorite" element={<Favorite />} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
-    </>
+    </HashRouter>
   );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { persistor, store } from './redux/store.js';
 import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <HashRouter basename="/car-rental">
+  <HashRouter>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <App />
